cfe9eada304cf54d0df576e72addb4e255761222
ShubhamIT109 <94740769+ShubhamIT109@users.noreply.github.com>
2023-12-11 17:30:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";//treat all js code as newer version 
+// alert(3+3) //it will not work here as we are using node not browser
+let name = "shubham";// string
+let age =  22;//number 
+let ans =  true; // boolean 
+let temperature = null;
+let sir = undefined;
+// number,binint, string,boolean,null,undefined,symbol
+//object
+
+console.log(typeof age);
