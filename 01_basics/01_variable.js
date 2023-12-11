c3c12267be1c35d45d25bb19d32e8c3518626dd1
ShubhamIT109 <94740769+ShubhamIT109@users.noreply.github.com>
2023-12-11 17:04:11 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 144553;
+let accountEmail="shubham@gmail.com"
+var accountPassword="12345" //prefer not to use var because of block and functional scope issue
+accountCity="Jaipur"
+let accountState;//undefined when value not assigned
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
