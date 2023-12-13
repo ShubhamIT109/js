e111e82ac3b6bf504c47acb143d0b3d31850265a
ShubhamIT109 <94740769+ShubhamIT109@users.noreply.github.com>
2023-12-13 17:05:39 +0000

    math

---
+++ 01_basics/05_math.js
@@ -0,0 +1,25 @@
+const score = 50;
+const score2 = new Number(100)
+//console.log(score2) 
+//console.log(score2.toString().length)
+//console.log(score2.toFixed(2))
+
+const otherNumber = 123.3793
+//console.log(otherNumber.toPrecision(3))
+const rupee = 1000000
+// console.log(rupee.toLocaleString('en-In'))
+
+///////////////// maths /////////////////
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+console.log(Math.min(2,5,4,7,1))
+console.log(Math.max(4,2,8,9,1))
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10+1))
+
+const min =10
+const max=20
+console.log(Math.floor(Math.random()* (max-min+1))+min)
