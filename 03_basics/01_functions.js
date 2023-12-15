68e231440f0ab20d0a4d1529410beb693db2e0c6
ShubhamIT109 <94740769+ShubhamIT109@users.noreply.github.com>
2023-12-15 15:43:49 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function sayMyName(){
+console.log("s")
+console.log("h")
+console.log("u")
+console.log("b")
+console.log("h")
+console.log("a")
+console.log("m")
+}
+
+// sayMyName()
+
+function addTwoNumber(number1 ,number2){
+return number1+number2
+}
+
+let result = addTwoNumber(3,5)
+// console.log("Result :",result)
+
+function userLoginMessage(username){
+    return `${username} welcome to my page`
+}
+console.log(userLoginMessage("shubham"))
