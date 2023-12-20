c48836775c5c90b696ab3017d9448ce729f96c25
ShubhamIT109 <94740769+ShubhamIT109@users.noreply.github.com>
2023-12-20 06:34:30 +0000

    small change

--- 05_iteration/four.js
+++ 05_iteration/four.js
@@ -1,9 +1,23 @@
 const coding = ["java","python","java script","c++"]
 
-coding.forEach(function (val){ //call back function
-    //console.log(val)
+coding.forEach(function (val,index,arr){ //call back function
+    // console.log(val,index,arr)
 })
 
 coding.forEach((val) => { //arrow function
-console.log(val)
+// console.log(val)
+})
+
+const myCod = [
+    {subjectName : "java",
+     price : 4000},
+     {subjectName : "python",
+     price : 3000},
+     {subjectName : "java Script",
+     price : 3500}
+
+]
+
+myCod.forEach((val)=>{
+ console.log(val.subjectName)
 })
